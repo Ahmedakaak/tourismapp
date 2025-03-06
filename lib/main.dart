0ac9541f858e7df2a979034eea135ab4e8d06847
Ahmed Akaak <akaakahmed52@gmail.com>
2025-03-06 13:55:51 +0400

    add colum and image

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(const MyApp());
 }
+
 class MyApp extends StatefulWidget {
   const MyApp({super.key});
 
@@ -13,12 +14,9 @@ class MyApp extends StatefulWidget {
 class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: MyHome()
-    );
+    return MaterialApp(home: MyHome());
   }
-
-} 
+}
 
 class MyHome extends StatelessWidget {
   const MyHome({super.key});
@@ -27,9 +25,33 @@ class MyHome extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("tourism app",style: TextStyle(color: const Color.fromARGB(255, 77, 53, 75)),),
-        backgroundColor: const Color.fromARGB(255, 200, 228, 252),
+        title: Text(
+          "tourism app",
+          style: TextStyle(color: const Color.fromARGB(255, 34, 44, 34)),
+        ),
+        backgroundColor: const Color.fromARGB(255, 48, 119, 48),
         centerTitle: true,
+      ),
+      body: Column(
+        children: [
+          Container(
+            padding: EdgeInsets.all(20),
+            color: const Color.fromARGB(255, 176, 255, 179),
+            child: Image(image: AssetImage('images/Salalah1.jpg')),
+          ),
+
+          Row(
+            children: [
+              Expanded(
+                child: SizedBox(
+                  width: 200,
+                  child: Image(image: AssetImage("images/Salalah2.jpg")),
+                ),
+              ),
+              Expanded(child: Image(image: AssetImage("images/Salalah3.jpg"))),
+            ],
+          ),
+        ],
       ),
     );
   }
