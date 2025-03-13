7d153c5f8e688cd5aa749f462a34fa471271f1e1
AhmedAkaak <akaakahmed52l@gmail.com>
2025-03-13 10:34:10 +0400

    Update appbar title to 'tourism app'

--- lib/main.dart
+++ lib/main.dart
@@ -14,11 +14,7 @@ class MyApp extends StatefulWidget {
 class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
-<<<<<<< HEAD
     return const MaterialApp(home: MyHome());
-=======
-    return MaterialApp(home: MyHome());
->>>>>>> 91de86e2abbc78c6c3e959c0488a4c2a6be3838f
   }
 }
 
@@ -29,15 +25,9 @@ class MyHome extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-<<<<<<< HEAD
         title: const Text(
-          "tourism app 1",
-          style: TextStyle(color: Color.fromARGB(255, 34, 44, 34)),
-=======
-        title: Text(
           "tourism app",
-          style: TextStyle(color: const Color.fromARGB(255, 34, 44, 34)),
->>>>>>> 91de86e2abbc78c6c3e959c0488a4c2a6be3838f
+          style: TextStyle(color: Color.fromARGB(255, 34, 44, 34)),
         ),
         backgroundColor: const Color.fromARGB(255, 48, 119, 48),
         centerTitle: true,
@@ -45,28 +35,16 @@ class MyHome extends StatelessWidget {
       body: Column(
         children: [
           Container(
-<<<<<<< HEAD
             padding: const EdgeInsets.all(20),
             color: const Color.fromARGB(255, 176, 255, 179),
             child: const Image(image: AssetImage('images/Salalah1.jpg')),
           ),
           const Text(
-=======
-            padding: EdgeInsets.all(20),
-            color: const Color.fromARGB(255, 176, 255, 179),
-            child: Image(image: AssetImage('images/Salalah1.jpg')),
-          ),
-          Text(
->>>>>>> 91de86e2abbc78c6c3e959c0488a4c2a6be3838f
-            'Popular Places',
+            'Popular Places in Oman',
             style: TextStyle(fontSize: 20.0, fontWeight: FontWeight.bold),
           ),
 
-<<<<<<< HEAD
           const Row(
-=======
-          Row(
->>>>>>> 91de86e2abbc78c6c3e959c0488a4c2a6be3838f
             children: [
               Expanded(
                 child: SizedBox(
@@ -80,44 +58,27 @@ class MyHome extends StatelessWidget {
           Container(
             width: double.infinity,
             color: const Color.fromARGB(255, 40, 121, 43),
-<<<<<<< HEAD
             child: const Text(
-=======
-            child: Text(
->>>>>>> 91de86e2abbc78c6c3e959c0488a4c2a6be3838f
               textAlign: TextAlign.center,
               'featerd place',
               style: TextStyle(
                 fontSize: 20.0,
                 fontWeight: FontWeight.bold,
-<<<<<<< HEAD
                 color: Color.fromARGB(255, 255, 255, 255),
-=======
-                color: const Color.fromARGB(255, 255, 255, 255),
->>>>>>> 91de86e2abbc78c6c3e959c0488a4c2a6be3838f
               ),
             ),
           ),
           Container(
             color: const Color.fromARGB(255, 187, 187, 187),
-<<<<<<< HEAD
             child: const Image(image: AssetImage("images/Salalah4.jpg")),
-=======
-            child: Image(image: AssetImage("images/Salalah4.jpg")),
->>>>>>> 91de86e2abbc78c6c3e959c0488a4c2a6be3838f
           ),
         ],
       ),
       floatingActionButton: FloatingActionButton(
         tooltip: 'Test',
         backgroundColor: const Color.fromARGB(255, 114, 155, 115),
-<<<<<<< HEAD
         foregroundColor: const Color.fromARGB(255, 248, 248, 248),
         child: const Icon(Icons.home),
-=======
-        foregroundColor: Color.fromARGB(255, 248, 248, 248),
-        child: Icon(Icons.home),
->>>>>>> 91de86e2abbc78c6c3e959c0488a4c2a6be3838f
         onPressed: () {
           print("welcome home");
         },
